7f846c8e53fcabb62c826ccd7de9d1483decfec0
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-10-01 22:45:25 -0600

    Updates

---
+++ precache-manifest.2709a58e8a355332cdf42fee49f56825.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150d9822d0c39f5f9931b70724861171",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ae1448c6b96c54aab5c",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "8d8a8de14c6f2735d000",
+    "url": "/static/css/main.f606cf7c.chunk.css"
+  },
+  {
+    "revision": "5ae1448c6b96c54aab5c",
+    "url": "/static/js/2.b94c19be.chunk.js"
+  },
+  {
+    "revision": "40fc3b0b2f311b53724923e80a9b150e",
+    "url": "/static/js/2.b94c19be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8a8de14c6f2735d000",
+    "url": "/static/js/main.64deae5c.chunk.js"
+  },
+  {
+    "revision": "ab8597d6873dee80b5e4",
+    "url": "/static/js/runtime-main.ca07173e.js"
+  },
+  {
+    "revision": "77c58bd437c3547b9f67897f2f16e61d",
+    "url": "/static/media/Andy.77c58bd4.jpeg"
+  },
+  {
+    "revision": "25ecac8a0d03efeb5017f7906b0502f6",
+    "url": "/static/media/Camila.25ecac8a.JPG"
+  },
+  {
+    "revision": "0195ca2864b1c610476950ebb495db7e",
+    "url": "/static/media/Logo-horizontal.0195ca28.png"
+  },
+  {
+    "revision": "e1013cda9190344d3207fc425cc93d48",
+    "url": "/static/media/Luca.e1013cda.jpg"
+  },
+  {
+    "revision": "8b08c95da0e1b599e7f43dd1f57707d6",
+    "url": "/static/media/Marco.8b08c95d.jpg"
+  },
+  {
+    "revision": "3484ba02c293b1d7ef29151fb3edb464",
+    "url": "/static/media/Maria.3484ba02.jpeg"
+  },
+  {
+    "revision": "d565691a242411cdb03e9d10934845c8",
+    "url": "/static/media/Willi.d565691a.jpg"
+  },
+  {
+    "revision": "cba5440efc8694aa6620d8d4b34ea675",
+    "url": "/static/media/fondoResponsive.cba5440e.jpg"
+  },
+  {
+    "revision": "b21042d1f21c0c87388468c7c5693f27",
+    "url": "/static/media/fondoResponsive2.b21042d1.jpg"
+  },
+  {
+    "revision": "1aec563b3c465a5fbef239ac9ed22d7e",
+    "url": "/static/media/fondoResponsive3.1aec563b.jpg"
+  }
+]);
